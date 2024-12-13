a508114e4fea643196aee36254a81cecc70acd65
alex <inrag3x@gmail.com>
2024-12-13 21:26:17 +0300

    ADD: enemy health view

---
+++ Assets/_Project/Runtime/Infrastructure/Factories/Point.cs
@@ -0,0 +1,15 @@
+﻿using _Project.Runtime.Core.Herbalist;
+using UnityEngine;
+
+namespace _Project.Runtime.Infrastructure.Factories
+{
+    public class Point : MonoBehaviour, ITransformable
+    {
+        public Transform Transform { get; private set; }
+
+        private void Awake()
+        {
+            Transform = transform;
+        }
+    }
+}
