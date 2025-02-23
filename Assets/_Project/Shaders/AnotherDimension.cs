5f98fcc664e7a31e14e3cffb80356a22453f97eb
Сушко Андрей <frosty.priense@gmail.com>
2025-02-23 11:39:08 +0300

    global world change

--- Assets/_Project/Shaders/AnotherDimension.cs
+++ Assets/_Project/Shaders/AnotherDimension.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using _Project.Runtime.Core.Herbalist;
+using _Project.Runtime.Core.Herbalist.GlobalWorldChange;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -10,19 +12,21 @@ using Zenject;
 public class AnotherDimension : MonoBehaviour
 {
     private Material _material;
-    [SerializeField] private int _radius = 2;
+    //[SerializeField] private int _radius = 2;
 
     private readonly Vector4[] _grenadesPositions = new Vector4[15];
     private int _grenadeCount;
     private IPotionExplosionProvider _provider;
+    private IGlobalWorldChangeSubscriptable _globalWorldChange;
     private static readonly int NumberGrenade = Shader.PropertyToID("_NumberGrenade");
     private static readonly int GrenadesPositions = Shader.PropertyToID("_GrenadesPositions");
     private static readonly int Dist = Shader.PropertyToID("_Dist");
 
     [Inject]
-    private void Construct(IPotionExplosionProvider provider)
+    private void Construct(IPotionExplosionProvider provider, IGlobalWorldChangeSubscriptable globalWorldChange)
     {
         _provider = provider;
+        _globalWorldChange = globalWorldChange;
     }
 
     private void Awake()
@@ -33,6 +37,7 @@ public class AnotherDimension : MonoBehaviour
     private void OnEnable()
     {
         _provider.GrenadesUpdated += OnGrenadesUpdated;
+        _globalWorldChange.OnGlobalWorldChange += OnGlobalWorldChange;
     }
 
     private void OnGrenadesUpdated(IReadOnlyList<GrenadeExplosion> grenadeExplosions)
@@ -41,14 +46,29 @@ public class AnotherDimension : MonoBehaviour
         {
             _grenadesPositions[i] = grenadeExplosions[i].transform.position;
         }
+        
+        var radius = grenadeExplosions.Count > 0 ?grenadeExplosions.Select(e => e.Radius).Max() : 1;
 
         _material.SetInt(NumberGrenade, grenadeExplosions.Count);
         _material.SetVectorArray(GrenadesPositions, _grenadesPositions);
-        _material.SetFloat(Dist, _radius);
+        _material.SetFloat(Dist, radius);
+    }
+
+    private void OnGlobalWorldChange(GrenadeExplosion explosion)
+    {
+        if (explosion is null)
+        {
+            OnGrenadesUpdated(new List<GrenadeExplosion>());
+        }
+        else
+        {
+            OnGrenadesUpdated(new List<GrenadeExplosion>{explosion});
+        }
     }
 
     private void OnDisable()
     {
         _provider.GrenadesUpdated -= OnGrenadesUpdated;
+        _globalWorldChange.OnGlobalWorldChange -= OnGlobalWorldChange;
     }
 }
