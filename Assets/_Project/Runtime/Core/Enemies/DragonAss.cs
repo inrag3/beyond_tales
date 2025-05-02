c8e829f0cc6480154aab9a194109c6236cb8c627
Сушко Андрей <frosty.priense@gmail.com>
2025-05-02 13:34:20 +0300

    dragon ass fix

--- Assets/_Project/Runtime/Core/Enemies/DragonAss.cs
+++ Assets/_Project/Runtime/Core/Enemies/DragonAss.cs
@@ -1,24 +1,26 @@
-﻿using _Project.Runtime.Core.Interactables;
+﻿using System;
+using System.Collections.Generic;
+using _Project.Runtime.Core.Interactables;
 using _Project.Runtime.QuestSystem;
 using UnityEngine;
 
 namespace _Project.Runtime.Core.Enemies
 {
-    public class DragonAss :Creature
+    public class DragonAss : Creature
     {
-        [SerializeField]
-        private DialogueInteractionTrigger _dialogue;
-        [SerializeField]
-        private BaseQuestAction _onHit;
-        
-        
+        [SerializeField] private DialogueInteractionTrigger _dialogue;
+        [SerializeField] private List<BaseQuestAction> _onHit;
+
+        private int counter = 0;
+
         private void Start()
         {
             this.Hit += () =>
             {
                 Debug.Log(name + " hit");
                 _dialogue.Interact();
-                _onHit?.Activate();
+                _onHit[counter]?.Activate();
+                counter = Math.Max(counter + 1, _onHit.Count - 1);
             };
         }
     }
