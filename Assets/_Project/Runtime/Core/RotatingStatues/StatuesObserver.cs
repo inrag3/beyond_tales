a959b2343b4babd9baeb6410e7a486f628d7f115
Сушко Андрей <frosty.priense@gmail.com>
2025-05-09 13:58:33 +0300

    idol quest

---
+++ Assets/_Project/Runtime/Core/RotatingStatues/StatuesObserver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using _Project.Runtime.Core.Interactables;
+using UnityEngine;
+using Zenject;
+
+namespace _Project.Runtime.Core.RotatingStatues
+{
+    public class StatuesObserver:MonoBehaviour
+    {
+        [SerializeField] private RotatingStatuesManipulator[] _statues;
+        [SerializeField] private Door _door;
+
+        private void Start()
+        {
+            foreach (var statue in _statues)
+            {
+                statue.OnStatueRotated += OnStatuesRotated;
+            }
+        }
+
+        private void OnStatuesRotated()
+        {
+            Debug.Log("Rotating statues");
+            if (_statues.All(s => s.Fixed))
+            {
+                Debug.Log("Rotating statues all");
+                OnAllBedsCompleted();
+            }
+        }
+
+        private void OnAllBedsCompleted()
+        {
+            _door.Open();
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var statue in _statues)
+            {
+                statue.OnStatueRotated -= OnStatuesRotated;
+            }
+        }
+    }
+}
