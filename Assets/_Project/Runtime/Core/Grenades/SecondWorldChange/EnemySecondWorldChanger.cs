a606e1f1ad26356b834da13e1333e8f73da77033
Сушко Андрей <frosty.priense@gmail.com>
2025-05-19 23:13:09 +0300

    EnemyChange

--- Assets/_Project/Runtime/Core/Grenades/SecondWorldChange/EnemySecondWorldChanger.cs
+++ Assets/_Project/Runtime/Core/Grenades/SecondWorldChange/EnemySecondWorldChanger.cs
@@ -1,3 +1,5 @@
+using System;
+using _Project.Runtime.Core.Enemies;
 using UnityEngine;
 
 namespace _Project.Runtime.Core.Grenades
@@ -5,19 +7,30 @@ namespace _Project.Runtime.Core.Grenades
     public class EnemySecondWorldChanger : SecondWorldExChangingTrigger
     {
         private bool _isChanged = false;
+        private float _sizeMod = 1.5f;
+        private float _hpMod = 2f;
+        [SerializeField] private GameObject obj;
+        [SerializeField] private Creature _health;
+
+        private Vector3 _startScale;
+
+        private void Start()
+        {
+            _startScale = obj.transform.localScale;
+        }
+
         protected override void TriggerWorldChange()
         {
-            if (!_isChanged)
-            {
-                //gameObject.GetComponent<Renderer>().material.color = Color.white;
-                Debug.Log("Имеджинируем партиклы превращения в другое животное");
-                gameObject.transform.localScale *= 0.5f;
-                _isChanged = true;
-            }
+            obj.gameObject.transform.localScale /= _sizeMod;
+            var currentHealth = _health.Health.Value.CurrentValue;
+            _health.Health.Decrease(currentHealth - currentHealth / _hpMod);
         }
 
         protected override void TriggerWorldChangeBack()
         {
+            obj.gameObject.transform.localScale = _startScale;
+            var currentHealth = _health.Health.Value.CurrentValue;
+            _health.Health.Increase(currentHealth * _hpMod - currentHealth);
         }
     }
 }
