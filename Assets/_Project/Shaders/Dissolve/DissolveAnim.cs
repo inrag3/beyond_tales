0b8336f8534cdc1bc145d75feccc1ff8607a0d24
senyak <mselina@sfedu.ru>
2025-05-21 20:02:27 +0300

    improving script for animation of dissolve

--- Assets/_Project/Shaders/Dissolve/DissolveAnim.cs
+++ Assets/_Project/Shaders/Dissolve/DissolveAnim.cs
@@ -1,62 +1,81 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class DissolveAnim : MonoBehaviour
 { 
-    private const float MaxTreshhold = 1.01f;
-    private const float MinTreshhold = -0.2f;
-    private const string  TresholdKey = "_Edge";
-    private MeshRenderer _renderer;
+    [Header("Dissolve Settings")]
+    [Tooltip("MinTreshhold")]
+    public float MinTreshhold = -0.2f;
 
+    [Tooltip("MaxTreshhold")]
+    public float MaxTreshhold = 1.01f;
+
+    [Tooltip("AnimationSpeed")]
+    public float AnimationSpeed = 0.5f;
+    private const string TresholdKey = "_Edge";
+    
+    private MeshRenderer _renderer;
+    private Material _dissolveMaterial;
     private Coroutine _show;
 
     private void Awake()
     {
         _renderer = GetComponent<MeshRenderer>();
+        _dissolveMaterial = _renderer.sharedMaterials[0];
+        
+        if(_dissolveMaterial == null)
+            Debug.LogError("Dissolve material not found!");
     }
+
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.F))
         {
-            if(_show != null)
-                StopCoroutine(_show);
-
-            _show = StartCoroutine(Dissolve());
-
+            StartDissolveCoroutine(Dissolve());
         }
         else if (Input.GetKeyUp(KeyCode.G))
         {
-            if(_show != null)
-                StopCoroutine(_show);
-
-            _show = StartCoroutine(Appear());
-            
+            StartDissolveCoroutine(Appear());
         }
+    }
+
+    private void StartDissolveCoroutine(IEnumerator routine)
+    {
+        if(_show != null)
+            StopCoroutine(_show);
+        
+        _show = StartCoroutine(routine);
     }
 
     private IEnumerator Dissolve()
     {
-        float treshold = 0;
+        float treshold = _dissolveMaterial.GetFloat(TresholdKey);
         while (treshold < MaxTreshhold)
         {
-            treshold += Time.deltaTime/2;
-            _renderer.material.SetFloat(TresholdKey, treshold);
+            treshold += Time.deltaTime * AnimationSpeed;
+            _dissolveMaterial.SetFloat(TresholdKey, treshold);
             yield return null;
         }
+        _dissolveMaterial.SetFloat(TresholdKey, MaxTreshhold);
     }
     
-    IEnumerator Appear()
+    private IEnumerator Appear()
     {
-        float treshold = 1;
+        float treshold = _dissolveMaterial.GetFloat(TresholdKey);
         while (treshold >= MinTreshhold)
         {
-            treshold -= Time.deltaTime/2;
-            _renderer.material.SetFloat(TresholdKey, treshold);
+            treshold -= Time.deltaTime * AnimationSpeed;
+            _dissolveMaterial.SetFloat(TresholdKey, treshold);
             yield return null;
         }
+        _dissolveMaterial.SetFloat(TresholdKey, MinTreshhold);
     }
     
+    private void OnDestroy()
+    {
+        if(_dissolveMaterial != null)
+            _dissolveMaterial.SetFloat(TresholdKey, MinTreshhold);
+    }
 }
